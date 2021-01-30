9fc9d7fedd36bb5575f320e2ad8016bd69f26dc2
Vikram Chatterjee <strycora@gmail.com>
2021-01-29 20:57:52 -0700

    saved user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :usergroups
+  has_many :workoutgroups, through: :usergroupss
   validates :username, presence: true, uniqueness: true
   has_secure_password
   
