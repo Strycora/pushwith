46461d7069452ee3c50af05d06ab6905855dab8f
Vikram Chatterjee <strycora@gmail.com>
2021-01-29 19:48:16 -0700

    added navbar and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def render_nav_links(signed_in)
+    content_tag("ul", class: "right") do
+      if signed_in
+        content_tag("li") do 
+          link_to("Workouts", workouts_path)
+        end +
+        content_tag("li") do
+          link_to("Create Workout", new_workout_path)
+        end +
+        content_tag("li") do 
+          link_to("Logout", destroy_user_session_path, method: "DELETE")
+        end
+      else
+        content_tag("li") do
+          link_to("Login", login_path)
+        end + 
+        content_tag("li") do 
+          link_to("Sign Up", signup_path)
+        end
+      end
+    end
+  end
 end
