3793cc4092771bb2e797571f5ae7d02d73e70716
Vikram Chatterjee <strycora@gmail.com>
2021-02-04 16:58:07 -0700

    refactored login user to DRY up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      login_user
       redirect_to workout_groups_path
     else
       flash.now[:error] = @user.errors.full_messages
